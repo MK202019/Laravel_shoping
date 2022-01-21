7a67b2ca351613449193d2d7b5eaede65286ed5d
MK202019 <91296207+MK202019@users.noreply.github.com>
2022-01-21 12:09:04 +0900

    ss

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\Admin\ItemController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
+
+
+Route::get('/about',function(){
+    return view('about');
+});
+
+Route::get('/admin/item',[ItemController::class, 'index']);
+Route::get('/admin/item/create',[ItemController::class, 'create']);
+Route::post('/admin/item/add',[ItemController::class, 'add']);
