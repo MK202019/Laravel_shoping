5231d0bf7690fb46330747bcc6d3dd435bf8f5f3
MK202019 <91296207+MK202019@users.noreply.github.com>
2022-01-21 13:27:07 +0900

    syu2

--- resources/views/admin/item/create.blade.php
+++ resources/views/admin/item/create.blade.php
@@ -1,1 +1,8 @@
 <h1>商品入力</h1>
+<a href="{{ route('admin.item.index') }}">商品一覧</a>
+
+<form action="{{ route('admin.item.add')}}" method="post">
+    <label for="">商品名</label>
+    <input type="text" name="name">
+    <button>更新</button>
+</form>
