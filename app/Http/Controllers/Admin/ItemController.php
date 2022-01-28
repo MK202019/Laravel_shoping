42022ccd8adc20728a2db03225e2720e534d0078
MK202019 <91296207+MK202019@users.noreply.github.com>
2022-01-28 11:14:46 +0900

    df

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -8,29 +8,47 @@ use App\Models\Item;
 
 class ItemController extends Controller
 {
+    //商品一覧ページ
     public function index()
     {
+        //SELECT * FROM items;
         $items = Item::get();
-        $data = ['items'=> $items];
-        return view('admin.item.index',$data);
+        $data = ['items' => $items];
+        return view('admin.item.index', $data);
     }
 
+    //商品入力
     public function create()
     {
         return view('admin.item.create');
     }
 
+    //商品追加  
     public function add(Request $request)
     {
         $posts = $request->all();
+        // dd($posts);
+        //INSERT INTO items SET name = 'XXXX', code ='XXXX', ...;
         Item::create($posts);
-        return redirect('/admin/item');
+        //商品一覧にリダイレクト
+        return redirect('/admin/item/');
     }
 
     public function edit(Request $request, $id)
     {
+        //SELECT * FROM items WHERE id = xxx;
         $item = Item::find($id);
-        $data = ['item => $item'];
-        return view('admin.item.edit',$data);
+        $data = ['item' => $item];
+        return view('admin.item.edit', $data);
     }
+
+    public function update(Request $request, $id)
+    {
+        $posts = $request->all();
+        //UPDATE items SET name = 'xxxx', code = 'xxxx' ... WHERE id = xxx;
+        Item::find($id)->update($posts);
+        return redirect()->route('admin.item.index');
+    }
+
+
 }
--- resources/views/admin/item/create.blade.php
+++ resources/views/admin/item/create.blade.php
@@ -1,23 +1,21 @@
-<h1>商品入力</h1>
-<a href="{{ route('admin.item.index') }}">商品一覧</a>
+<h2>{{ __('Item Input') }}</h2>
+<a href="{{ route('admin.item.index') }}">{{ __('Back') }}</a>
 
-<form action="{{ route('admin.item.add')}}" method="post">
-    @csrf
+<form action="{{ route('admin.item.add') }}" method="post">
+  @csrf
 
-    <div>
-        <label for="">{{ __('Item Name') }}</label>
-        <input type="text" name="name">
-    </div>
-    <div>
-        <label for="">{{ __('Item Code') }}</label>
-        <input type="text" name="code">
-    </div>
-    <div>
-        <label for="">{{ __('Price') }}</label>
-        <input type="number" name="price">
-    </div>
+  <div>
+    <label for="">{{ __('label.item_name') }}</label>
+    <input type="text" name="name">
+  </div>
+  <div>
+    <label for="">{{ __('label.item_code') }}</label>
+    <input type="text" name="code">
+  </div>
+  <div>
+    <label for="">{{ __('label.price') }}</label>
+    <input type="number" name="price">
+  </div>
 
-
-
-    <button>{{ __('Update') }}</button>
+  <button>{{ __('Update') }}</button>
 </form>
